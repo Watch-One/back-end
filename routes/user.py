a81652199eebee6af21324311a304997a4976152
Lucas Pintos <lucaspintos909@gmail.com>
2022-05-01 18:22:39 -0300

    Devolviendo usuarios de ejemplo en endpoint /users

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+from config.db import connection
+from models.user import users
 
 user = APIRouter()
 
 @user.get("/users")
 def get_users():
-    return []
+    return connection.execute(users.select()).fetchall()
