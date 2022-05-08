2b5c0499d844a1ddeb6daf55dda2dbbebc0492b0
Lucas Pintos <lucaspintos909@gmail.com>
2022-05-08 15:24:47 -0300

    Dejando un todo en el modelo para no olvidarnos

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Table
 from sqlalchemy.sql.sqltypes import Integer, String
 from config.db import meta, engine
 
+# TODO: Agregar lenguaje, pais y proveedores al modelo de usuario
 users = Table("users", meta,
               Column("id", Integer, primary_key=True),
               Column("name", String(255)),
