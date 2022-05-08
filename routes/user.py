6bc7213ce5b8dce38a3a2002fef1ff207b5bc03a
Lucas Pintos <lucaspintos909@gmail.com>
2022-05-08 16:40:19 -0300

    Ruta y funcionalidad para crear usuarios

--- routes/user.py
+++ routes/user.py
@@ -1,9 +1,26 @@
 from fastapi import APIRouter
 from config.db import connection
 from models.user import users
+from schemas.user import User
+from cryptography.fernet import Fernet
 
 user = APIRouter()
 
-@user.get("/users")
-def get_users():
-    return connection.execute(users.select()).fetchall()
+# Generando key aleatoria para encriptar las contraseñas
+key = Fernet.generate_key()
+f = Fernet(key)
+
+# Ruta para crear usuarios
+@user.post("/user/create")
+def create_user(user: User):
+    #Creo objeto con datos del usuario
+    new_user = {"name": user.name,
+                "email": user.email,
+                # Encriptando la contraseña
+                "password": f.encrypt(user.password.encode("utf-8"))}
+
+    # Ingreso al usuario a la DB
+    result = connection.execute(users.insert().values(new_user))
+
+    # Consulto el usuario ingresado a la DB y lo devuelvo
+    return connection.execute(users.select().where(users.c.id == result.lastrowid)).first()
