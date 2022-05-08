81652b76d8619e060c3ddaabd7b8a22fa733852c
Lucas Pintos <lucaspintos909@gmail.com>
2022-05-08 18:31:37 -0300

    Importando y agregando las rutas para peliculas

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.user import user as user_routes
+from routes.movie import movie as movie_routes
 
 app = FastAPI()
 
 app.include_router(user_routes)
+app.include_router(movie_routes)
