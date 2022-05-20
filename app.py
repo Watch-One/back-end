f9cdb59541402dc6fbfc0d118948d72fe3c3794e
Lucas Pintos <lucaspintos909@gmail.com>
2022-05-19 22:54:07 -0300

    Configurando cors para localhost

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.user import user as user_routes
 from routes.movie import movie as movie_routes
 
 app = FastAPI(
     title="WatchOne Backend"
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user_routes)
