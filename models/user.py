ae906b890a8793ce6c8a3030613a7055fd5ec1b3
lucaspintos909 <lucaspintos909@gmail.com>
2022-05-26 18:39:48 -0300

    feat: Agregando lenguaje al modelo de usuario

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Table
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.sqltypes import Enum, Integer, String
 from config.db import meta, engine
 
 # TODO: Agregar lenguaje, pais y proveedores al modelo de usuario
@@ -7,6 +7,7 @@ users = Table("users", meta,
               Column("id", Integer, primary_key=True),
               Column("name", String(255)),
               Column("email", String(255)),
-              Column("password", String(255)))
+              Column("password", String(255)),
+              Column("lang", Enum('en', 'es', 'br')))
 
 meta.create_all(engine)
