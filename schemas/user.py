5928c9c092f86ba9f4420ea65536a3fc89ba2641
lucaspintos909 <lucaspintos909@gmail.com>
2022-06-02 16:05:16 -0300

    feat: Usando tipo de dato EmailStr para mejorar verificacion

--- schemas/user.py
+++ schemas/user.py
@@ -1,9 +1,19 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class User(BaseModel):
     id: Optional[str]
     name: str
-    email: str
+    email: EmailStr
     password: str
     lang: str
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Johnny Depp",
+                "email": "jhonny@deep.com",
+                "password": "strongpassword",
+                "lang": "es"
+            }
+        }
