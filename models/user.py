ee757e0a3eef0edd988aa047b792a290ca7623f7
lucaspintos909 <lucaspintos909@gmail.com>
2022-06-07 17:30:04 -0300

    fix: Dejando el email como valor unico

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from config.db import meta, engine
 userModel = Table("users", meta,
               Column("id", Integer, primary_key=True),
               Column("name", String(255)),
-              Column("email", String(255)),
+              Column("email", String(255), unique=True),
               Column("password", String(255)),
               Column("lang", Enum('en', 'es', 'br')))
 
