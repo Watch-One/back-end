8cb6c5679d479d61153c4417f1566dbc749f3f1d
lucaspintos909 <lucaspintos909@gmail.com>
2022-06-08 13:32:38 -0300

    feat: Agregando dependencia de autenticacion a ruta para obtener usuario

--- routes/user.py
+++ routes/user.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
 from config.db import connection
 from models.user import userModel
 from schemas.user import UserSchema, UserLoginSchema
 from cryptography.fernet import Fernet
 from auth.jwtHandler import signJWT
+from auth.authBearer import JWTBearer
 from schemas.user import UserLoginResponseSchema
 from decouple import config
 
@@ -13,10 +14,11 @@ usersRouter = APIRouter()
 key = bytes(config("SECRET_TOKEN"), 'utf-8')
 fernet = Fernet(key)
 
+
 # Ruta para obtener usuario por id
-def getUserByEmail(email: str):
-    user = connection.execute(userModel.select().where(userModel.c.email == email)).first()
-    return user if user else False
+@usersRouter.get("/user/{id}", dependencies=[Depends(JWTBearer())],response_model=UserSchema, tags=["Users"])
+def get_user(id: str):
+    return "getUserById(id)"
 
 # Ruta para crear usuarios
 @usersRouter.post("/user/signup", tags=["Users"])
@@ -38,8 +40,17 @@ def login(user: UserLoginSchema = Body(default=None)):
     user_exists = getUserByEmail(user.email)
 
     if not user_exists or not bytes(user.password, 'utf-8') == fernet.decrypt(bytes(user_exists.password,'utf-8')):
-        return {"message": "¡Usuario o contraseña incorrectas!"}
+        return {"detail": "¡Usuario o contraseña incorrectas!"}
 
-    result = {"message": "¡Inicio de sesión exitoso!", "token": signJWT(user_exists.email)["access_token"]}
-    
+    result = {"detail": "¡Inicio de sesión exitoso!", "token": signJWT(user_exists.email)["access_token"]}
+
     return result
+
+
+def getUserByEmail(email: str):
+    user = connection.execute(userModel.select().where(userModel.c.email == email)).first()
+    return user if user else False
+
+def getUserById(id: str):
+    user = connection.execute(userModel.select().where(userModel.c.id == id)).first()
+    return user if user else False
